fed16a6db93994c996689d478f19e561a45bdf52
OChriiis <herickandersondossantos01234@hotmail.com>
2021-06-30 07:59:43 -0300

    Atualização

---
+++ App/Controller/Produtos.php
@@ -0,0 +1,14 @@
+<?php
+use App\Core\Controller;
+
+class Produtos extends Controller{
+    
+    public function index(){
+
+        $produtoModel = $this->model("Produto");
+
+        $dados = $produtoModel->listarTodos();
+
+        $this->view("produtos/index", $dados);
+    }
+}
